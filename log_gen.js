d7d1ed1a9e7b1392d5b61815a04ea2002374d29b
teraton <henri.terho@gmail.com>
2018-02-04 15:56:41 +0200

    added a tester gen
    added small log generator to generate a test file

---
+++ log_gen.js
@@ -0,0 +1,38 @@
+//test filegenerator
+
+let error_messages = [
+    "error1",
+    "error2",
+    "error3",
+    "error4"
+]
+
+//generate the weird timestamp format
+
+function generate_error(){
+
+    let now = new Date;
+    let date_part = now.getFullYear() + "-" + now.getMonth() + "-" + now.getDate();
+    let time_part = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+    let random_error = error_messages[Math.floor(Math.random()*error_messages.length)];
+
+    return date_part + " " + time_part + " " + random_error;
+    
+}
+
+//save a new line to a file randomly 
+var gen_func = function writeErrorToFile(){    
+    var fs = require('fs');
+    let error = generate_error();
+    fs.appendFile("test.txt", error + "\r\n", function(err) {
+        if(err) {
+            return console.log(err);
+        }
+
+        console.log("Generated error: " + error);
+    }); 
+    setTimeout(gen_func, Math.random()* 10 * 1000);
+}
+
+console.log("started Gen");
+setTimeout(gen_func, Math.random()* 10 * 1000 );
